454e5329c47de48e26d428ede23611a3a524395e
DataScienceNow <david.salvador@sofwerx.org>
2018-05-01 15:01:20 -0400

    add

--- __init__.py
+++ __init__.py
@@ -15,11 +15,11 @@ from mycroft.util.log import LOG
 # from the MycroftSkill class.  You extend this class as shown below.
 
 # TODO: Change "Template" to a unique name for your skill
-class TemplateSkill(MycroftSkill):
+class davidsSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        super(TemplateSkill, self).__init__(name="TemplateSkill")
+        super(davidsSkill, self).__init__(name="davidsSkill")
         
         # Initialize working variables used within the skill.
         self.count = 0
@@ -62,4 +62,4 @@ class TemplateSkill(MycroftSkill):
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
-    return TemplateSkill()
+    return davidsSkill()
